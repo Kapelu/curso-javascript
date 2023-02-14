4073d3f4438b7718f428292cf264c3e6a9222333
Kapelu <ddaniel_calderon@hotmail.com>
2023-02-13 23:30:11 -0300

    upDate 13/02/23

--- FT-M4/02-sql/homework/01 - Exercises/script/Utils/firstLetterToCapital.js
+++
@@ -1,8 +0,0 @@
-module.exports = (models) => {
-   let entries = Object.entries(models);
-   let capsEntries = entries.map((entry) => [
-      entry[0][0].toUpperCase() + entry[0].slice(1),
-      entry[1],
-   ]);
-   return Object.fromEntries(capsEntries);
-};
