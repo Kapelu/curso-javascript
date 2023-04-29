5437361793d87962c2fb72bb4d9387b3c7204c65
Daniel Calderon <ddaniel_calderon@hotmail.com>
2023-04-29 20:00:55 -0300

    Update 29-04-2023

---
+++ FT-M3/CheckPoint/2023-04-14-kapelu/routes/14-route.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/controllers');
+// No modifiques nada arriba de esta línea
+
+// Escribe la lógica de las rutas acá
+// router.put('/cats/add-accessory', (req, res) => {})
+
+// No modifiques nada debajo de esta línea
+module.exports = router;
