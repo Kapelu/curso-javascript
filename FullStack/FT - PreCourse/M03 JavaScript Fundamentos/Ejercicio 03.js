c56dec60751e561c1808ca993d2bfe5dcbe7c734
kapelu <ddaniel_calderon@hotmail.com>
2024-02-09 23:46:32 -0300

    ✔ Completado el 09-02-2024

--- FullStack/FT - PreCourse/M03 JavaScript Fundamentos/Ejercicio 03.js
+++ FullStack/FT - PreCourse/M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,42 +5,42 @@ function sonIguales(x, y) {
 	// Retorna True si "x" e "y" son iguales.
 	// De lo contrario, retorna False.
 	// Tu código:
-	return x === y ? true : false
+	//return x === y ? true : false
 }
 
 function tienenMismaLongitud(str1, str2) {
 	// Retorna True si los dos strings tienen la misma longitud.
 	// De lo contrario, retorna False.
 	// Tu código:
-	return str1.length === str2.length ? true : false
+	//return str1.length === str2.length ? true : false
 }
 
 function menosQueNoventa(num) {
 	// Retorna True si el argumento "num" es menor que noventa.
 	// De lo contrario, retorna False.
 	// Tu código:
-	return num < 90 ? true : false
+	//return num < 90 ? true : false
 }
 
 function mayorQueCincuenta(num) {
 	// Retorna True si el argumento "num" es mayor que cincuenta.
 	// De lo contrario, retorna False.
 	// Tu código:
-	return num > 50 ? true : false
+	//return num > 50 ? true : false
 }
 
 function esPar(num) {
 	// Retorna True si "num" es par.
 	// De lo contrario, retorna False.
 	// Tu código:
-	return num % 2 === 0 ? true : false
+	//return num % 2 === 0 ? true : false
 }
 
 function esImpar(num) {
 	// Retorna True si "num" es impar.
 	// De lo contrario, retorna False.
 	// Tu código:
-	return num % 2 !== 0 ? true : false
+	//return num % 2 !== 0 ? true : false
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
